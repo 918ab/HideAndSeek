b63351173e7e5cf8111902c1c5031d75c38049f6
918 <rmarudxo05@gmail.com>
2025-05-06 22:47:02 +0900

    2025-05-06(pm)

--- src/main/java/main/hideandseek/Command/HideAndSeekCommand.java
+++ src/main/java/main/hideandseek/Command/HideAndSeekCommand.java
@@ -10,8 +10,30 @@ import org.jetbrains.annotations.NotNull;
 public class HideAndSeekCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        Player player =  (Player) sender;
-        ModelEnginePlay.ModelPlay(player,"shift");
-        return false;
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("플레이어만 이 명령어를 실행할 수 있습니다.");
+            return false;
+        }
+
+        Player player = (Player) sender;
+
+        // 애니메이션 실행
+        ModelEnginePlay.ModelPlay(player, "shift");
+
+        // 인자가 존재하는지 확인
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("true")) {
+                // idle 애니메이션을 멈추고 새로운 애니메이션 실행
+                ModelEnginePlay.ModelStop(player, "idle");
+            } else if (args[0].equalsIgnoreCase("false")) {
+                // 기본 idle 애니메이션 실행
+                ModelEnginePlay.ModelStop(player, "shift");
+            } else {
+                player.sendMessage("유효한 인자를 입력해주세요. 'true' 또는 'false'만 가능합니다.");
+            }
+        } else {
+            player.sendMessage("인자를 입력하세요. 예: /명령어 true 또는 /명령어 false");
+        }
+        return true;
     }
 }
--- src/main/java/main/hideandseek/HideAndSeek.java
+++ src/main/java/main/hideandseek/HideAndSeek.java
@@ -1,7 +1,9 @@
 package main.hideandseek;
 
-import com.ticxo.modelengine.api.ModelEngineAPI;
+
+
 import main.hideandseek.Command.HideAndSeekCommand;
+import main.hideandseek.Static.ModelEnginePlay;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,13 +12,8 @@ public final class HideAndSeek extends JavaPlugin {
     @Override
     public void onEnable() {
         Bukkit.getLogger().info("[HideAndSeek] Plugin Enable");
-        ModelEngineAPI modelEngine = ModelEngineAPI.getAPI();
-        if (modelEngine == null) {
-            Bukkit.getLogger().info("[HideAndSeek] ModelEngine 플러그인 찾을 수 없음");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
         getCommand("숨바꼭질").setExecutor(new HideAndSeekCommand());
+        ModelEnginePlay.startCustomModeMonitor();
     }
 
     @Override
